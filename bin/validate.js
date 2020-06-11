f54d5af79a358137a64eff23a455203822dd11f3
Johan Stokking <johan@stokking.nl>
2020-06-11 20:51:14 +0200

    Add validation logic

--- bin/validate.js
+++ bin/validate.js
@@ -1,31 +1,72 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs')
-const fs = require('fs')
-const Ajv = require('ajv')
-const yaml = require('js-yaml')
+const Ajv = require('ajv');
+const yargs = require('yargs');
+const fs = require('fs');
+const yaml = require('js-yaml');
 
-const schema = require('../schema.json')
+let validate = new Ajv().addSchema([require('../lib/draft/schema.json'), require('../schema.json')]);
 
-const options = yargs
-  .usage('Usage: --definition <definition> --data <file>')
-  .option('definition', {
-    describe: 'Definition',
-    type: 'string',
-    demandOption: true
-  })
-  .option('data', {
-    describe: 'Path to data file',
-    type: 'string',
-    demandOption: true
-  })
-  .argv;
+const options = yargs.usage('Usage: --vendor <file>').option('v', {
+  alias: 'vendor',
+  describe: 'Path to vendor index file',
+  type: 'string',
+  demandOption: true,
+  default: './vendor/index.yaml',
+}).argv;
 
-const data = yaml.safeLoad(fs.readFileSync(options.data))
-const validate = ajv.addSchema(schema).compile({ $ref: `https://lorawan-schema.org/draft/devices/1/schema#/definitions/${options.definition}` })
-if (!validate(obj)) {
-  console.log(`${options.data} is invalid`)
-  console.log(validate.errors)
-  process.exit(1)
+let validateVendors = validate.compile({
+  $ref: 'https://schema.thethings.network/devicerepository/1/schema#/definitions/vendors',
+});
+let validateVendor = validate.compile({
+  $ref: 'https://schema.thethings.network/devicerepository/1/schema#/definitions/vendor',
+});
+let validateEndDevice = validate.compile({
+  $ref: 'https://lorawan-schema.org/draft/devices/1/schema#/definitions/endDevice',
+});
+let validateEndDeviceProfile = validate.compile({
+  $ref: 'https://lorawan-schema.org/draft/devices/1/schema#/definitions/endDeviceProfile',
+});
+
+const vendors = yaml.safeLoad(fs.readFileSync(options.vendor));
+
+if (!validateVendors(vendors)) {
+  console.log(`${options.vendor} is invalid`);
+  throw new Error(validateVendors.errors);
 }
-console.log(`${options.data} is valid`)
+console.log(`vendor: valid`);
+
+vendors.vendors.forEach((v) => {
+  const vendor = yaml.safeLoad(fs.readFileSync(`./vendor/${v.id}/index.yaml`));
+  if (!validateVendor(vendor)) {
+    console.log(`${v.id}: index valid`);
+    throw new Error(validateVendors.errors);
+  }
+  console.log(`${v.id}: valid index`);
+
+  let profiles = {};
+
+  vendor.endDevices.forEach((d) => {
+    const endDevice = yaml.safeLoad(fs.readFileSync(`./vendor/${v.id}/${d}.yaml`));
+    if (!validateEndDevice(endDevice)) {
+      console.log(`${v.id}: ${d}: invalid`);
+      throw new Error(validateVendors.errors);
+    }
+    console.log(`${v.id}: ${d}: valid`);
+
+    endDevice.firmwareVersions.forEach((version) => {
+      Object.keys(version.profiles).forEach((region) => {
+        const id = version.profiles[region].id;
+        if (!profiles[id]) {
+          const profile = yaml.safeLoad(fs.readFileSync(`./vendor/${v.id}/${id}.yaml`));
+          if (!validateEndDeviceProfile(profile)) {
+            console.log(`${v.id}: ${d}: profile ${id} invalid`);
+            throw new Error(validateVendors.errors);
+          }
+          profiles[id] = true;
+        }
+        console.log(`${v.id}: ${d}: profile ${id} (${region}) valid`);
+      });
+    });
+  });
+});
