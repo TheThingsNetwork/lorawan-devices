cae8ab088af9c91f623c177cd5ffed61109dd204
TeneoIoT <info@teneo-iot.nl>
2021-01-12 16:09:17 +0100

    Added the CO2 stoplicht and filling degree sensor nodes for Teneo IoT

---
+++ vendor/teneo-iot/co2-stoplicht.js
@@ -0,0 +1,45 @@
+function Decoder(bytes, port) {
+  var decoded = {};
+  var index = 0;
+  decoded.Voltage = (bytes[index++] & 0x0F) / 10 + 2;
+  
+  if(port == 223)
+  {
+    if((bytes[0] & 0xc0) == 0x80)
+    {
+      decoded.type = "status";
+        decoded.status = bytes[0] & ~0xC0;
+    }
+    
+    return decoded;
+  }
+  
+  
+  if(port == 1)
+  {
+      decoded.CO2 = (bytes[2] << 24 | bytes[3] << 16 | bytes[4] << 8 | bytes[5])/100;
+    
+     var Tempx100 = ((bytes[6] << 8) + bytes[7]);
+     
+     if (Tempx100 == 32767)
+    {
+      decoded.Temp = "no temp"
+    }
+    else
+    {
+      if (Tempx100 > 32767)
+      {
+        decoded.Temp = ((-(65536 - Tempx100)))/100;
+      } 
+      else
+      {
+        decoded.Temp = Tempx100/100; 
+      }
+    }
+
+    
+     decoded.Humid = (bytes[8] << 8 | bytes[9])/100;
+  }
+  
+  return decoded;
+  }
---
+++ vendor/teneo-iot/filling-degree-sensor.js
@@ -0,0 +1,50 @@
+function Decoder(bytes, port)
+{
+  var decoded = {};
+  decoded.sensorType = 'fillrate';
+  
+  if (bytes.length === 0) 
+  {
+    decoded.valid = false;
+    return decoded;
+  }
+  
+  decoded.settingsAllowed = true;
+  decoded.charging = false;
+  decoded.battery = 2 + (bytes[0] / 10);
+  
+  if (port === 1)
+  {
+    if (bytes.length === 3)
+    {
+	  decoded.valid = true;
+      decoded.distance = (bytes[1] << 8) | bytes[2];
+    }
+    else
+    {
+	  decoded.valid = false;
+      decoded.errorcode = -1;
+    }
+  }
+  else if (port === 2)
+  {
+	var code = bytes[1]
+	if(code === 4)
+	{
+	  decoded.valid = true;
+	  decoded.distance = -1;
+	}
+	else
+	{
+	  decoded.valid = false;
+      decoded.errorcode = bytes[1];
+	}
+  }
+  else if (port === 3)
+  {
+    decoded.valid = false;
+    decoded.charging = true;
+  }
+
+  return decoded;
+}
