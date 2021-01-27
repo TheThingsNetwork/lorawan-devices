8000cff03f02ddd125abfcf86da2c5a7c6baa5e8
Johan Stokking <johan@stokking.nl>
2021-01-27 14:21:28 +0100

    Fix codec

--- vendor/teneo-iot/co2-stoplicht.js
+++ vendor/teneo-iot/co2-stoplicht.js
@@ -1,7 +1,7 @@
 function decodeUplink(input) {
   var data = {};
   var index = 0;
-  data.Voltage = (input.bytes[index++] & 0x0f) / 10 + 2;
+  data.battery = (input.bytes[index++] & 0x0f) / 10 + 2;
 
   if (input.fPort == 223) {
     if ((input.bytes[0] & 0xc0) == 0x80) {
@@ -13,21 +13,21 @@ function decodeUplink(input) {
   }
 
   if (input.fPort == 1) {
-    data.CO2 = ((input.bytes[2] << 24) | (input.bytes[3] << 16) | (input.bytes[4] << 8) | input.bytes[5]) / 100;
+    data.co2 = ((input.bytes[2] << 24) | (input.bytes[3] << 16) | (input.bytes[4] << 8) | input.bytes[5]) / 100;
 
     var Tempx100 = (input.bytes[6] << 8) + input.bytes[7];
 
     if (Tempx100 == 32767) {
-      data.Temp = 'no temp';
+      data.temperature = 'no temp';
     } else {
       if (Tempx100 > 32767) {
-        data.Temp = -(65536 - Tempx100) / 100;
+        data.temperature = -(65536 - Tempx100) / 100;
       } else {
-        data.Temp = Tempx100 / 100;
+        data.temperature = Tempx100 / 100;
       }
     }
 
-    data.Humid = ((input.bytes[8] << 8) | input.bytes[9]) / 100;
+    data.relativeHumidity = ((input.bytes[8] << 8) | input.bytes[9]) / 100;
   }
 
   return {
