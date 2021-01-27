262483e341cdce65b01a244c97245caa2790025d
Johan Stokking <johan@stokking.nl>
2021-01-27 13:27:34 +0100

    Fix port and format

--- vendor/teneo-iot/co2-stoplicht.js
+++ vendor/teneo-iot/co2-stoplicht.js
@@ -1,47 +1,36 @@
 function decodeUplink(input) {
   var data = {};
   var index = 0;
-  data.Voltage = (input.bytes[index++] & 0x0F) / 10 + 2;
-  
-  if(input.port == 223)
-  {
-    if((input.bytes[0] & 0xc0) == 0x80)
-    {
-      data.type = "status";
-        data.status = input.bytes[0] & ~0xC0;
+  data.Voltage = (input.bytes[index++] & 0x0f) / 10 + 2;
+
+  if (input.fPort == 223) {
+    if ((input.bytes[0] & 0xc0) == 0x80) {
+      data.type = 'status';
+      data.status = input.bytes[0] & ~0xc0;
     }
-    
+
     return data;
   }
-  
-  
-  if(input.port == 1)
-  {
-      data.CO2 = (input.bytes[2] << 24 | input.bytes[3] << 16 | input.bytes[4] << 8 | input.bytes[5])/100;
-    
-     var Tempx100 = ((input.bytes[6] << 8) + input.bytes[7]);
-     
-     if (Tempx100 == 32767)
-    {
-      data.Temp = "no temp"
-    }
-    else
-    {
-      if (Tempx100 > 32767)
-      {
-        data.Temp = ((-(65536 - Tempx100)))/100;
-      } 
-      else
-      {
-        data.Temp = Tempx100/100; 
+
+  if (input.fPort == 1) {
+    data.CO2 = ((input.bytes[2] << 24) | (input.bytes[3] << 16) | (input.bytes[4] << 8) | input.bytes[5]) / 100;
+
+    var Tempx100 = (input.bytes[6] << 8) + input.bytes[7];
+
+    if (Tempx100 == 32767) {
+      data.Temp = 'no temp';
+    } else {
+      if (Tempx100 > 32767) {
+        data.Temp = -(65536 - Tempx100) / 100;
+      } else {
+        data.Temp = Tempx100 / 100;
       }
     }
 
-    
-     data.Humid = (input.bytes[8] << 8 | input.bytes[9])/100;
+    data.Humid = ((input.bytes[8] << 8) | input.bytes[9]) / 100;
   }
-  
+
   return {
     data: data,
   };
-  }
+}
--- vendor/teneo-iot/filling-degree-sensor.js
+++ vendor/teneo-iot/filling-degree-sensor.js
@@ -1,51 +1,38 @@
-function decodeUplink(input)
-{
+function decodeUplink(input) {
   var data = {};
   data.sensorType = 'fillrate';
-  
-  if (input.bytes.length === 0) 
-  {
+
+  if (input.bytes.length === 0) {
     data.valid = false;
     return data;
   }
-  
+
   data.settingsAllowed = true;
   data.charging = false;
-  data.battery = 2 + (input.bytes[0] / 10);
-  
-  if (input.port === 1)
-  {
-    if (input.bytes.length === 3)
-    {
-	  data.valid = true;
+  data.battery = 2 + input.bytes[0] / 10;
+
+  if (input.fPort === 1) {
+    if (input.bytes.length === 3) {
+      data.valid = true;
       data.distance = (input.bytes[1] << 8) | input.bytes[2];
-    }
-    else
-    {
-	  data.valid = false;
+    } else {
+      data.valid = false;
       data.errorcode = -1;
     }
-  }
-  else if (input.port === 2)
-  {
-	var code = input.bytes[1]
-	if(code === 4)
-	{
-	  data.valid = true;
-	  data.distance = -1;
-	}
-	else
-	{
-	  data.valid = false;
+  } else if (input.fPort === 2) {
+    var code = input.bytes[1];
+    if (code === 4) {
+      data.valid = true;
+      data.distance = -1;
+    } else {
+      data.valid = false;
       data.errorcode = input.bytes[1];
-	}
-  }
-  else if (input.port === 3)
-  {
+    }
+  } else if (input.fPort === 3) {
     data.valid = false;
     data.charging = true;
   }
-  
+
   return {
     data: data,
   };
