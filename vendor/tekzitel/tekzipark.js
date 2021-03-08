3e90f02d9883c966b204fea5af10650561a150bb
Arif <arif.mansoori@tekzitel.com>
2021-03-08 22:35:42 +1100

    Add TEKZIPARK device

--- vendor/tekzitel/tekzipark.js
+++ vendor/tekzitel/tekzipark.js
@@ -6,20 +6,21 @@ function decodeUplink(input) {
   var beacons = [];
 
   switch (input.fPort) {
-    case 1:
-      data.occupied = input.bytes[0] & 0x80;
+    case 1:  
+      var index = 0;    
+      data.occupied = (input.bytes[0] & 0x80) >> 7;
       data.keepAlive = input.bytes[0] & 0x01;
-      data.reset = input.bytes[0] & 0x02;
-      data.No_Beacon = input.bytes[0] & 0x04;
-      data.Radar = input.bytes[0] & 0x08;
-      data.Obstruction = input.bytes[0] & 0x10;
-      data.Good_Battery = input.bytes[0] & 0x20;
+      data.reset = (input.bytes[0] & 0x02) >> 1;
+      data.No_Beacon = (input.bytes[0] & 0x04) >> 2;
+      data.Radar = (input.bytes[0] & 0x08) >> 3;
+      data.Obstruction = (input.bytes[0] & 0x10) >> 4;
+      data.Good_Battery = (input.bytes[0] & 0x20) >> 5;
       data.Temperature = (input.bytes[1] > 0x7F) ? input.bytes[1] - 0x100 : input.bytes[1];
       data.Parking_ID = input.bytes[2];
       if (input.bytes.length > 3) {
         data.Beacon_RSSI = (input.bytes[3] > 0x7F) ? input.bytes[3] - 0x100 : input.bytes[3];
         beaconcount = (input.bytes.length - 4) / 2;        
-        for (let index = 4; index < input.bytes.length; index++) {
+        for (index = 4; index < input.bytes.length; index++) {
           beacons = beacons.concat((input.bytes[index] << 8) | input.bytes[index + 1]);
           index++;
         }
@@ -27,13 +28,13 @@ function decodeUplink(input) {
       }
       break;
     case 2:
-      data.occupied = input.bytes[0] & 0x80;
+      data.occupied = (input.bytes[0] & 0x80) >> 7;
       data.keepAlive = input.bytes[0] & 0x01;
-      data.reset = input.bytes[0] & 0x02;
-      data.No_Beacon = input.bytes[0] & 0x04;
-      data.Radar = input.bytes[0] & 0x08;
-      data.Obstruction = input.bytes[0] & 0x10;
-      data.Good_Battery = input.bytes[0] & 0x20;
+      data.reset = (input.bytes[0] & 0x02) >> 1;
+      data.No_Beacon = (input.bytes[0] & 0x04) >> 2;
+      data.Radar = (input.bytes[0] & 0x08) >> 3;
+      data.Obstruction = (input.bytes[0] & 0x10) >> 4;
+      data.Good_Battery = (input.bytes[0] & 0x20) >> 5;
       data.Temperature = (input.bytes[1] > 0x7F) ? input.bytes[1] - 0x100 : input.bytes[1];
       data.Parking_ID = input.bytes[2];
       data.Deflection_X = (input.bytes[3] > 0x7F) ? input.bytes[3] - 0x100 : input.bytes[3];
@@ -72,7 +73,7 @@ function encodeDownlink(input) {
   ret = ret.concat(byte0, byte1, byte2, byte3, byte4, byte5, byte6, byte7);
   return {
     // LoRaWAN FPort used for the downlink message
-    fPort: 0,
+    fPort: 3,
     // Encoded bytes
     bytes: ret
   };
@@ -81,40 +82,40 @@ function encodeDownlink(input) {
 function decodeDownlink(input) {
   var downlinkdata = {};
   switch (input.fPort) {
-    case 0:
+    case 3:
       
       downlinkdata.fluctuations_count = input.bytes[0] & 0x0F;
-      downlinkdata.radar_start_range = input.bytes[0] & 0x70;
-      downlinkdata.suppress_baseline_offset = input.bytes[0] & 0x80;
+      downlinkdata.radar_start_range = (input.bytes[0] & 0x70) >> 4;
+      downlinkdata.suppress_baseline_offset = (input.bytes[0] & 0x80) >> 7;
 
       downlinkdata.deflection_threshold_mag = input.bytes[1] & 0x0F;
-      downlinkdata.debug_mode = input.bytes[1] & 0x10;
-      downlinkdata.axis_deflection = input.bytes[1] & 0x60;
-      downlinkdata.downlink_frequency = input.bytes[1] & 0x80;
+      downlinkdata.debug_mode = (input.bytes[1] & 0x10) >> 4;
+      downlinkdata.axis_deflection = (input.bytes[1] & 0x60) >> 5;
+      downlinkdata.downlink_frequency = (input.bytes[1] & 0x80) >> 7;
 
       downlinkdata.fluctuation_threshold_XY = input.bytes[2] & 0x07;
-      downlinkdata.hw_acc_samples_no = input.bytes[2] & 0x18;
-      downlinkdata.oversampling = input.bytes[2] & 0x60;
-      downlinkdata.zero_neg_drop = input.bytes[2] & 0x80;
+      downlinkdata.hw_acc_samples_no = (input.bytes[2] & 0x18) >> 3;
+      downlinkdata.oversampling = (input.bytes[2] & 0x60) >> 5;
+      downlinkdata.zero_neg_drop = (input.bytes[2] & 0x80) >> 7;
 
       downlinkdata.fluctuation_delay_time = input.bytes[3] & 0x07;
-      downlinkdata.mag_reboot = input.bytes[3] & 0x08;
-      downlinkdata.reset_control = input.bytes[3] & 0x10;
-      downlinkdata.sleep_time = input.bytes[3] & 0xE0;
+      downlinkdata.mag_reboot = (input.bytes[3] & 0x08) >> 3;
+      downlinkdata.reset_control = (input.bytes[3] & 0x10) >> 4;
+      downlinkdata.sleep_time = (input.bytes[3] & 0xE0) >> 5;
 
       downlinkdata.keepalive_period = input.bytes[4] & 0x03;
-      downlinkdata.self_heal = input.bytes[4] & 0x04;
-      downlinkdata.fluctuation_threshold_Z = input.bytes[4] & 0x38;
-      downlinkdata.axis_fluctuations = input.bytes[4] & 0xC0;
+      downlinkdata.self_heal = (input.bytes[4] & 0x04) >> 2;
+      downlinkdata.fluctuation_threshold_Z = (input.bytes[4] & 0x38) >> 3;
+      downlinkdata.axis_fluctuations = (input.bytes[4] & 0xC0) >> 6;
 
       downlinkdata.reflection_threshold_radar = input.bytes[5] & 0x07;
-      downlinkdata.obstruction_check = input.bytes[5] & 0x08;
-      downlinkdata.radar_scan_count = input.bytes[5] & 0xF0;
+      downlinkdata.obstruction_check = (input.bytes[5] & 0x08) >> 3;
+      downlinkdata.radar_scan_count = (input.bytes[5] & 0xF0) >> 4;
 
       downlinkdata.ble_scan_window = input.bytes[6] & 0x07;
-      downlinkdata.beacons_scan_no = input.bytes[6] & 0x38;
-      downlinkdata.send_rssi = input.bytes[6] & 0x40;
-      downlinkdata.OI = input.bytes[6] & 0x80;
+      downlinkdata.beacons_scan_no = (input.bytes[6] & 0x38) >> 3;
+      downlinkdata.send_rssi = (input.bytes[6] & 0x40) >> 6;
+      downlinkdata.OI = (input.bytes[6] & 0x80) >> 7;
 
       downlinkdata.ble_scan_rssi = input.bytes[7] & 0xFF;
       
