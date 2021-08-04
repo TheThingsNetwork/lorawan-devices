65f1ca730699a0dad8cd50d1fc81beaffd6daee3
Jaime Trinidad <81958808+Jaime-Trinidad@users.noreply.github.com>
2021-08-04 05:48:20 -0500

    Netvox Devices (#273)
    * Add Netvox Devices
    
    add devices
    
    * add index

---
+++ vendor/netvox/payload/r718da2_r718db2_r718f2.js
@@ -0,0 +1,184 @@
+function getCfgCmd(cfgcmd){
+  var cfgcmdlist = {
+    1:   "ConfigReportReq",
+    129: "ConfigReportRsp",
+    2:   "ReadConfigReportReq",
+    130: "ReadConfigReportRsp"
+  };
+  return cfgcmdlist[cfgcmd];
+}
+
+function getDeviceName(dev){
+  var deviceName = {
+	47: "R718DA2",
+    61: "R718DB2",
+    62: "R718F2",
+    67: "R718J2",
+    69: "R718LB2",
+    76: "R311CA",
+    86: "R311CB",
+    126: "R730F2",
+    138: "R730DA2",
+    140: "R730DB2",
+    142: "R730LB2",
+    108: "R311CC"
+  };
+  return deviceName[dev];
+}
+
+function getCmdToID(cmdtype){
+  if (cmdtype == "ConfigReportReq")
+	  return 1;
+  else if (cmdtype == "ConfigReportRsp")
+	  return 129;
+  else if (cmdtype == "ReadConfigReportReq")
+	  return 2;
+  else if (cmdtype == "ReadConfigReportRsp")
+	  return 130;
+}
+
+function getDeviceType(devName){
+  if (devName == "R718DA2")
+	  return 47;
+  else if (devName == "R718DB2")
+	  return 61;
+  else if (devName == "R718F2")
+	  return 62;
+  else if (devName == "R718J2")
+	  return 67;
+  else if (devName == "R718LB2")
+	  return 69;
+  else if (devName == "R311CA")
+	  return 76;
+  else if (devName == "R311CB")
+      return 86;
+  else if (devName == "R730F2")
+      return 126;
+  else if (devName == "R730DA2")
+	  return 138;
+  else if (devName == "R730DB2")
+	  return 140;
+  else if (devName == "R730LB2")
+	  return 142;
+  else if (devName == "R311CC")
+      return 108;
+}
+
+function padLeft(str, len) {
+    str = '' + str;
+    if (str.length >= len) {
+        return str;
+    } else {
+        return padLeft("0" + str, len);
+    }
+}
+
+function decodeUplink(input) {
+  var data = {};
+  switch (input.fPort) {
+    case 6:
+		if (input.bytes[2] === 0x00)
+		{
+			data.Device = getDeviceName(input.bytes[1]);
+			data.SWver =  input.bytes[3]/10;
+			data.HWver =  input.bytes[4];
+			data.Datecode = padLeft(input.bytes[5].toString(16), 2) + padLeft(input.bytes[6].toString(16), 2) + padLeft(input.bytes[7].toString(16), 2) + padLeft(input.bytes[8].toString(16), 2);
+			
+			return {
+				data: data,
+			};
+		}
+		data.Device = getDeviceName(input.bytes[1]);
+		data.Volt = input.bytes[3]/10;
+		data.status1 = input.bytes[4];
+		data.status2 = input.bytes[5];
+		
+		break;
+		
+	case 7:
+		data.Device = getDeviceName(input.bytes[1]);
+		if (input.bytes[0] === 0x81)
+		{
+			data.Cmd = getCfgCmd(input.bytes[0]);
+			data.Status = (input.bytes[2] === 0x00) ? 'Success' : 'Failure';
+		}
+		else if (input.bytes[0] === 0x82)
+		{
+			data.Cmd = getCfgCmd(input.bytes[0]);
+			data.MinTime = (input.bytes[2]<<8 | input.bytes[3]);
+			data.MaxTime = (input.bytes[4]<<8 | input.bytes[5]);
+			data.BatteryChange = input.bytes[6]/10;
+		}
+		break;
+		
+	default:
+      return {
+        errors: ['unknown FPort'],
+      };
+	  
+    }
+          
+	  
+	 return {
+		data: data,
+	};
+ }
+  
+function encodeDownlink(input) {
+  var ret = [];
+  var devid;
+  var port;
+  var getCmdID;
+	  
+  getCmdID = getCmdToID(input.data.Cmd);
+  devid = getDeviceType(input.data.Device);
+
+  if (input.data.Cmd == "ConfigReportReq")
+  {
+	  var mint = input.data.MinTime;
+	  var maxt = input.data.MaxTime;
+	  var batteryChg = input.data.BatteryChange * 10;
+	  
+	  port = 7;
+	  ret = ret.concat(getCmdID, devid, (mint >> 8), (mint & 0xFF), (maxt >> 8), (maxt & 0xFF), batteryChg, 0x00, 0x00, 0x00, 0x00);
+  }
+  else if (input.data.Cmd == "ReadConfigReportReq")
+  {
+	  port = 7;
+	  ret = ret.concat(getCmdID, devid, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+  }  
+    
+  return {
+    fPort: port,
+    bytes: ret
+  };
+}  
+  
+function decodeDownlink(input) {
+  var data = {};
+  switch (input.fPort) {
+    case 7:
+		data.Device = getDeviceName(input.bytes[1]);
+		if (input.bytes[0] === getCmdToID("ConfigReportReq"))
+		{
+			data.Cmd = getCfgCmd(input.bytes[0]);
+			data.MinTime = (input.bytes[2]<<8 | input.bytes[3]);
+			data.MaxTime = (input.bytes[4]<<8 | input.bytes[5]);
+			data.BatteryChange = input.bytes[6]/10;
+		}
+		else if (input.bytes[0] === getCmdToID("ReadConfigReportReq"))
+		{
+			data.Cmd = getCfgCmd(input.bytes[0]);
+		}
+		break;
+		
+    default:
+      return {
+        errors: ['invalid FPort'],
+      };
+  }
+  
+  return {
+		data: data,
+	};
+}
