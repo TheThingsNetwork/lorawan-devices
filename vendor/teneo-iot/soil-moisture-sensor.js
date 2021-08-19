3acadc1bb37572dc9b1d7bf6a7d8887167f70d16
TeneoIoT <info@teneo-iot.nl>
2021-08-19 17:04:49 +0200

    Yet another attempt at fixing the payload decoder

--- vendor/teneo-iot/soil-moisture-sensor.js
+++ vendor/teneo-iot/soil-moisture-sensor.js
@@ -1,47 +1,46 @@
-function decodeUplink(input) {
+function decodeUplink(input){
 	var data = {};
 	data.sensorType = 'moisture';
 
-	if (input.bytes.length === 0) {
+	if (input.bytes.length === 0){
 		data.valid = false;
-		return {
+		return{
 			data: data
 		}
 	}
 
 	data.settingsAllowed = true;
-	data.moisture = [];
+	data.moisture = 0;
 	data.charging = false;
 	data.battery = 2 + (input.bytes[0] / 10);
 
-	if (input.port === 1) {
-		if (input.bytes.length == 3) {
+	if (input.port === 1){
+		if (input.bytes.length === 3){
 			data.valid = true;
-			data.moisture.push(input.bytes[1] << 0);
+			data.moisture = input.bytes[1];
 
 			var temp = input.bytes[2];
-			if (temp > 127)
-			{
+			if (temp > 127){
 				temp = temp - 256;
 			}
 			data.temperature = temp;
 		}
-		else if (input.bytes.length === 6) {
+		else if (input.bytes.length === 6){
 			data.valid = true;
-			data.moisture.push(input.bytes[1] << 0);
+			data.moisture = input.bytes[1];
 
 			var temp = input.bytes[2]<<24>>16 | input.bytes[3];
 			data.temperature = temp / 100;
-		} else {
+		} else{
 			data.valid = false;
 			data.errorcode = -1;
 		}
-	} else if (input.port === 3) {
+	} else if (input.port === 3){
 		data.valid = false;
 		data.charging = true;
 	}
 
-	return {
+	return{
 		data: data
 	}
 }
