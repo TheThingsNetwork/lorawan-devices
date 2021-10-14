49e4cf5ec712f8fc97e29af7170fc541315712d7
Xinxin Yang <sommerpi>
2021-10-14 13:11:11 +1100

    PLS2-L & PTS2-L both can return battery voltage, decoder can handle negative levels/pressure

--- vendor/ellenex/pls2-l.js
+++ vendor/ellenex/pls2-l.js
@@ -7,15 +7,25 @@ function decodeUplink(input) {
           errors: ['input bytes in wrong format'],
         };
       }
-      var k = 0.019
-      var m = 0.05
-      var b = 0
-      var d = 1   // Liquid density
-      var level = (k * parseInt((input.bytes[3]).toString(16) + (input.bytes[4]).toString(16), 16) * m + b) / d
+      var tmp = ('00'+(input.bytes[3]).toString(16)).slice(-2) + ('00'+(input.bytes[4]).toString(16)).slice(-2);
+      var pReading = 0;
+      // process negative pressure
+      if (input.bytes[3] === 0xFF) {
+        // fill in the first 16 bits with 1's to perform bitwise operation
+        pReading = - ~(parseInt('FFFF' + tmp.toString(16) , 16) - 1);
+      } else {
+        pReading = parseInt(tmp, 16);
+      }
+      // (K * Pressure * m + b) / Liquid density
+      // k = 0.019, m = 0.05, b = 0, liquid density = 1
+      // separate calibration sheet will specify K, m and b
+      var level = (0.019 * pReading * 0.05 + 0) / 1;
+      var battery = parseInt(input.bytes[7].toString(16), 16) * 0.1;
       return {
         // Decoded data
         data: {
           level: Math.round(level * 100, 2) / 100,
+          batteryVoltage: Math.round(battery * 10, 1) / 10,
         },
       };
     default:
--- vendor/ellenex/pts2-l.js
+++ vendor/ellenex/pts2-l.js
@@ -7,16 +7,25 @@ function decodeUplink(input) {
           errors: ['input bytes in wrong format'],
         };
       }
-      var k = 0.01907
-      var m = 0.05
-      var b = 0
-      var pressure = k * parseInt((input.bytes[3]).toString(16) + (input.bytes[4]).toString(16), 16) * m + b
-      var battery = parseInt(input.bytes[7].toString(16), 16) * 0.1
+      var tmp = ('00'+(input.bytes[3]).toString(16)).slice(-2) + ('00'+(input.bytes[4]).toString(16)).slice(-2);
+      var pReading = 0;
+      // process negative pressure
+      if (input.bytes[3] === 0xFF) {
+        // fill in the first 16 bits with 1's to perform bitwise operation
+        pReading = - ~(parseInt('FFFF' + tmp.toString(16) , 16) - 1);
+      } else {
+        pReading = parseInt(tmp, 16);
+      }
+      // K * Pressure * m + b
+      // K = 0.01907, m = 0.05, b = 0
+      // separate calibration sheet will specify K, m and b
+      var pressure = 0.01907 * pReading * 0.05 + 0;
+      var battery = parseInt(input.bytes[7].toString(16), 16) * 0.1;
       return {
         // Decoded data
         data: {
           pressure: Math.round(pressure * 100, 2) / 100,
-          batteryVoltage: Math.round(battery * 10, 1)/ 10,
+          batteryVoltage: Math.round(battery * 10, 1) / 10,
         },
       };
     default:
