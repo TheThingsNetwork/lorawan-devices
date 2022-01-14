a8c46fb6b43b585d5b395ca72fe65b3d11e1252f
Xinxin Yang <xinxin-yang@hotmail.com>
2022-01-14 16:21:44 +1100

    Add downlink encoder with a single js file

--- vendor/ellenex/pls2-l.js
+++ vendor/ellenex/pls2-l.js
@@ -1,47 +1,41 @@
 function decodeUplink(input) {
   switch (input.fPort) {
     case 1:
-      // return error if length of Bytes is not 8
+      // throw an error if length of Bytes is not 8
       if (input.bytes.length != 8) {
         return {
-          errors: ['input bytes in wrong format'],
+          errors: ['Invalid uplink payload: length is not 8 byte'],
         };
       }
-      let pressure = integerParser(input.bytes[3], input.bytes[4]);
-      // (K * Pressure * m + b) / Liquid density
-      // k = 0.019, m = 0.05, b = 0, liquid density = 1
-      // separate calibration sheet will specify K, m and b
-      let k = 0.019, m = 0.05, b = 0, density = 1;
-      let level = (k * pressure * m + b) / density;
-      let battery = input.bytes[7] * 0.1;
+      let level = readHex2bytes(input.bytes[3], input.bytes[4]);
+      let batteryVoltage = input.bytes[7] * 0.1;
       return {
-        // Decoded data
         data: {
-          level: Math.round(level * 100, 2) / 100,
-          batteryVoltage: Math.round(battery * 10, 1) / 10,
+          level: level,
+          batteryVoltage: +batteryVoltage.toFixed(1),
         },
-      };
+      }
+      break;
     default:
       return {
-        errors: ['unknown FPort'],
+        errors: ['Unknown FPort: please use fPort 1'],
       };
   }
 }
 
 /*
- * This function is to process a 16-bit integer signed by the leftmost bit
- * represented by 2 bytes. 
+ * The readHex2bytes function is to decode a signed 16-bit integer
+ * represented by 2 bytes.  
  */
-function integerParser(byte1, byte2) {
-  // check whether the byte is signed as a negative number
-  // by checking whether the significant bit (leftmost) is 1
-  let negative = byte1 & (1 << 7);
-  // merge the two bytes
-  let result = (byte1 << 8) | byte2;
-  // process negative integer
+function readHex2bytes(byte1, byte2) {
+  let result = (byte1 << 8) | byte2;  // merge the two bytes
+  // check whether input is signed as a negative number
+  // by checking whether significant bit (leftmost) is 1
+  let negative = byte1 & 0x80;
+  // process negative value
   if (negative) {
-    // minus 1 and flip the bits
-    result = ~(0xFFFF0000 | (result - 1)) * (-1);
+    //result = ~(0xFFFF0000 | (result - 1)) * (-1);  // minus 1 and flip all bits
+    result = result - 0x10000;
   }
   return result;
 }
