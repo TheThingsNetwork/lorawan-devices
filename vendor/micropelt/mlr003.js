b69f2a3cc6086b7502df6ce4da717e890d3bb2a3
Micropelt-EH4 <66784039+Micropelt-EH4@users.noreply.github.com>
2022-02-07 08:41:21 +0100

    MLR003 REV1.1: Uplink is now 11 bytes (#378)
    Micropelt MLR003 REV1.1: The Uplink length has increased from 10 bytes to 11 bytes. The Uplink now includes the User Mode and User Value.

--- vendor/micropelt/mlr003.js
+++ vendor/micropelt/mlr003.js
@@ -1,9 +1,8 @@
 function decodeUplink(input) {
   switch (input.fPort) {
     case 1:
-      return {
-        // Decoded data
-        data: {
+      {
+        var output = {
           Current_Valve_Position: input.bytes[0],
           Flow_Sensor_Raw: input.bytes[1]*0.5,
           Flow_Temperature: input.bytes[2]*0.5,
@@ -12,21 +11,34 @@ function decodeUplink(input) {
           Energy_Storage: input.bytes[5]>>6 & 0x01,
           Harvesting_Active: input.bytes[5]>>5 & 0x01,
           Ambient_Sensor_Failure: input.bytes[5]>>4 & 0x01,
-          Flow_Sensor_Failure : input.bytes[5]>>3 & 0x01,
+          Flow_Sensor_Failure: input.bytes[5]>>3 & 0x01,
           Radio_Communication_Error: input.bytes[5]>>2 & 0x01,
           Received_Signal_Strength: input.bytes[5]>>1 & 0x01,
           Motor_Error: input.bytes[5]>>0 & 0x01,
-          Storage_Voltage: input.bytes[6]*0.02,
+          Storage_Voltage: Number((input.bytes[6]*0.02).toFixed(2)),
           Average_Current_Consumed: input.bytes[7]*10,
           Average_Current_Generated: input.bytes[8]*10,
           Reference_Completed: input.bytes[9]>>4 & 0x01,
           Operating_Mode: input.bytes[9]>>7 & 0x01,
-          Storage_Fully_Charged : input.bytes[9]>>6 & 0x01
-        },
-      };
-    default:
-      return {
-        errors: ['unknown FPort'],
-      };
+          Storage_Fully_Charged: input.bytes[9]>>6 & 0x01,
+        }
+        if (input.bytes.length == 11) {
+          var um = input.bytes[9] & 0x03
+          if (um == 0) {
+            var uv = input.bytes[10]
+          }
+          else {
+            var uv = input.bytes[10]*0.5
+          }
+          output.User_Mode = um
+          output.User_Value = uv
+        }
+        return { data: output };
+      }
+      default:
+        return {
+          errors: ['unknown FPort'],
+        };
+      }
+
   }
-}
