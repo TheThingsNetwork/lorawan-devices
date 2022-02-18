5c7ea76b925abd61a82f2963082163aa0454925f
Pierre Fraboulet <pierre.fraboulet@st.com>
2022-02-18 18:10:35 +0100

    add new LoRaWAN_End_Node v1.2.0 Firmware version with LoRaWAN v1.0.4 support + add more data details for uplink decoder

--- vendor/stmicroelectronics/nucleo-wl55jc.js
+++ vendor/stmicroelectronics/nucleo-wl55jc.js
@@ -2,8 +2,30 @@ function decodeUplink(input) {
   var data = {};
   switch (input.fPort) {
     case 2:
-      data.light = input.bytes[0];
-      data.temperature = input.bytes[3] & 0x80 ? input.bytes[3] - 0x100 : input.bytes[3];
+      data.battery_voltage = {
+        displayName: 'Battery voltage',
+        unit: 'V',
+        value: (((input.bytes[6] * 1200) / 254) + 1800) / 1000
+      };
+      data.humidity = {
+        displayName: 'Relative humidity',
+        unit: '%',
+        value: ((input.bytes[4] << 8) + input.bytes[5]) / 10
+      };
+      data.light = {
+        displayName: 'Red led status',
+        value: (input.bytes[0] & 0x01) ? 'ON':'OFF'
+      };
+      data.pressure = {
+        displayName: 'Barometric pressure',
+        unit: 'Pa',
+        value: ((input.bytes[1] << 8) + input.bytes[2]) / 10
+      };
+      data.temperature = {
+        displayName: 'Internal temperature',
+        unit: '°C',
+        value: input.bytes[3] & 0x80 ? input.bytes[3] - 0x100 : input.bytes[3]
+      };
       return {
         data: data
       }
