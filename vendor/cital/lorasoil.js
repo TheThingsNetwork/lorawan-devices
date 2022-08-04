4b31d95d409b8daab137c49eba4c2ba0bbe552db
Andreas Senn <andreas.senn@valenco.ch>
2022-08-04 18:59:58 +0200

    Payload decoder

--- vendor/cital/lorasoil.js
+++ vendor/cital/lorasoil.js
@@ -1,22 +1,17 @@
-
-function decodeBytes(bytes) {
+function decodeBytesMeasurement(bytes) {
     var data = {};
 
-    // temperatures can be negative, Int16Array properly decodes a signed integer
     data.tempOffBoard = new Int16Array([bytes[0] << 8 | bytes[1]])[0] / 100;
     data.waterPotOffBoard = ((bytes[2] << 8) | bytes[3]) / -100;
     data.batteryCharge = (bytes[4]);
 
-    // only the last two numbers of the year are stored, together with the month (don't ask me why)
-    // the same with day/hour and minute/second, seems like people have never heard of epoche
     var year = Math.floor((bytes[5] << 8 | bytes[6]) / 100) + 2000;
     var month = parseInt((bytes[5] << 8 | bytes[6]).toString().substring(2, 4))-1;
-    // Note: AS: I added this from the TTS integration - still needed?
     if ((bytes[5] << 8 | bytes[6]) < 12) {
         month = "1";
     }
     var day = Math.floor((bytes[7] << 8 | bytes[8]) / 100);
-    // following would be the same
+
     var hour = (bytes[7] << 8 | bytes[8] ) - day * 100;
     var minute = Math.floor((bytes[9] << 8 | bytes[10]) / 100);
     var second = (bytes[9] << 8 | bytes[10]) - minute * 100;
@@ -32,8 +27,6 @@ function decodeBytes(bytes) {
         data.airPressOnBoard = (bytes[23] << 8 | bytes[24])
         data.humidityOnBoard = bytes[25];
     }
-    // Add current TS as tbProcessTs for Debugging
-    data.tbProcessTs = Math.round((new Date()).getTime());
     return {
         "ts": ts, 
         "values": data
@@ -42,49 +35,23 @@ function decodeBytes(bytes) {
 
 function decodeUplink(input) {
   switch (input.fPort) {
-    case 1:
+    case 152:
+      var decoded = decodeBytesMeasurement(input.bytes);
       return {
         // Decoded data
-        var decoded = decodeBytes(input.bytes);
         data: {
-          temperature: decoded.tempOffBoard,
-          moisture: decoded.waterPotOffBoard,
+          time_stamp: decoded.ts,
+          soil_temperature: decoded.values.tempOffBoard,
+          soil_moisture: decoded.values.waterPotOffBoard,
+          ambient_temperature: decoded.values.tempOnBoard,
+          ambient_humidity: decoded.values.humidityOnBoard,
+          latitude: decoded.values.lat,
+          longitude: decoded.values.long
         },
       };
     default:
       return {
         errors: ['unknown FPort'],
-      };
-  }
-}
-
-function encodeDownlink(input) {
-  var i = colors.indexOf(input.data.led);
-  if (i === -1) {
-    return {
-      errors: ['invalid LED color'],
-    };
-  }
-  return {
-    // LoRaWAN FPort used for the downlink message
-    fPort: 2,
-    // Encoded bytes
-    bytes: [i],
-  };
-}
-
-function decodeDownlink(input) {
-  switch (input.fPort) {
-    case 2:
-      return {
-        // Decoded downlink (must be symmetric with encodeDownlink)
-        data: {
-          led: colors[input.bytes[0]],
-        },
-      };
-    default:
-      return {
-        errors: ['invalid FPort'],
       };
   }
 }
