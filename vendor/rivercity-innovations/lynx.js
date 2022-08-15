40a79ba632195f419a0eb2451af9b4ab80133b2a
Riley Stevenson <riley@rivercityinnovations.ca>
2022-08-15 14:09:14 -0600

    replaced the correct bytes reference in lynx.js

--- vendor/rivercity-innovations/lynx.js
+++ vendor/rivercity-innovations/lynx.js
@@ -6,42 +6,42 @@ function decodeUplink(input) {
   {
     decoded.type = "position";
     
-    decoded.latitudeDeg = bytes[3] + bytes[2] * 256 + bytes[1] * 65536 + bytes[0] * 16777216; 
+    decoded.latitudeDeg = input.bytes[3] + input.bytes[2] * 256 + input.bytes[1] * 65536 + input.bytes[0] * 16777216; 
     if (decoded.latitudeDeg >= 0x80000000) // 2^31 
       decoded.latitudeDeg -= 0x100000000; // 2^32 
     decoded.latitudeDeg /= 1e6; 
     
-    decoded.longitudeDeg = bytes[7] + bytes[6] * 256 + bytes[5] * 65536 + bytes[4] * 16777216; 
+    decoded.longitudeDeg = input.bytes[7] + input.bytes[6] * 256 + input.bytes[5] * 65536 + input.bytes[4] * 16777216; 
     if (decoded.longitudeDeg >= 0x80000000) // 2^31 
       decoded.longitudeDeg -= 0x100000000; // 2^32 
     decoded.longitudeDeg /= 1e6; 
     
-    decoded.inTrip = ((bytes[8] & 0x80) !== 0) ? true : false;
-    decoded.fixFailed = ((bytes[8] & 0x40) !== 0) ? true : false; 
-    decoded.batV = ((bytes[8] & 0x3F) + 20) / 10; // 6 bits, range 0V to 6V 
+    decoded.inTrip = ((input.bytes[8] & 0x80) !== 0) ? true : false;
+    decoded.fixFailed = ((input.bytes[8] & 0x40) !== 0) ? true : false; 
+    decoded.batV = ((input.bytes[8] & 0x3F) + 20) / 10; // 6 bits, range 0V to 6V 
     
-    // decoded.speedKmph = (bytes[9] * 160 / 256); // 8 bits, range 0 to 160 km/h
-    decoded.direction = (bytes[9] * 360/255); // 8 bits, range 0 to (360-360/255) degrees
+    // decoded.speedKmph = (input.bytes[9] * 160 / 256); // 8 bits, range 0 to 160 km/h
+    decoded.direction = (input.bytes[9] * 360/255); // 8 bits, range 0 to (360-360/255) degrees
     
-    decoded.temperature = bytes[10];
+    decoded.temperature = input.bytes[10];
     if (decoded.temperature > 127) // negative
       decoded.temperature -= 128;
     decoded.temperature = decoded.temperature / 2;
   }
   else if(input.fPort == 3)
   {
-    decoded.LoRaWANVersion = bytes[0].toString(16) + "." + bytes[1].toString(16) + "." + bytes[2].toString(16) + "." + bytes[3].toString(16);
-    decoded.FirmwareVersion = bytes[4].toString(16) + "." + bytes[5].toString(16) + "." + bytes[6].toString(16) + "." + bytes[7].toString(16);
+    decoded.LoRaWANVersion = input.bytes[0].toString(16) + "." + input.bytes[1].toString(16) + "." + input.bytes[2].toString(16) + "." + input.bytes[3].toString(16);
+    decoded.FirmwareVersion = input.bytes[4].toString(16) + "." + input.bytes[5].toString(16) + "." + input.bytes[6].toString(16) + "." + input.bytes[7].toString(16);
   }
   else if (input.fPort == 4)
   {
-    decoded.ShortSleepTime = bytes[3] + bytes[2] * 256 + bytes[1] * 65536 + bytes[0] * 16777216;
-    decoded.LongSleepTime = bytes[7] + bytes[6] * 256 + bytes[5] * 65536 + bytes[4] * 16777216;
+    decoded.ShortSleepTime = input.bytes[3] + input.bytes[2] * 256 + input.bytes[1] * 65536 + input.bytes[0] * 16777216;
+    decoded.LongSleepTime = input.bytes[7] + input.bytes[6] * 256 + input.bytes[5] * 65536 + input.bytes[4] * 16777216;
   }
   else if (input.fPort == 5)
   {
-    decoded.gpsTimeoutSec = bytes[3] + bytes[2] * 256 + bytes[1] * 65536 + bytes[0] * 16777216;
-    decoded.accelSensitivity = bytes[4];
+    decoded.gpsTimeoutSec = input.bytes[3] + input.bytes[2] * 256 + input.bytes[1] * 65536 + input.bytes[0] * 16777216;
+    decoded.accelSensitivity = input.bytes[4];
   }
   return {
     data: decoded
