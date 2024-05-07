d10a9b9fbd5636ebe36b51991f0a48ea420b4f12
decentlab <17007399+decentlab@users.noreply.github.com>
2024-05-07 10:55:24 +0200

    Fix decentlab device product URLs

--- vendor/decentlab/dl-blg.js
+++ vendor/decentlab/dl-blg.js
@@ -1,5 +1,5 @@
 
-/* https://www.decentlab.com/black-globe-temperature-sensor-for-lorawan */
+/* https://www.decentlab.com/products/black-globe-temperature-sensor-for-lorawan */
 
 var decentlab_decoder = {
   PROTOCOL_VERSION: 2,
