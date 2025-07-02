86036f5ad28322b28b742f055aa85cfb2f547ef9
netvox-tech <support@netvox.com.tw>
2025-07-02 17:02:17 +0800

    add r900pd01, r900pd01o1

--- vendor/netvox/payload/r900pd01.js
+++ vendor/netvox/payload/r900pd01.js
@@ -8,17 +8,13 @@ function getCfgCmd(cfgcmd){
     131: "SetShockSensorSensitivityRsp",
     4:   "GetShockSensorSensitivityReq",
     132: "GetShockSensorSensitivityRsp",
-	5:   "SetSoilTypeReq",
-    133: "SetSoilTypeRsp",
-    6:   "GetSoilTypeReq",
-    134: "GetSoilTypeRsp",
 
-	7:   "ConfigDryContactPointOutReq",
-    135: "ConfigDryContactPointOutRsp",
-	8:   "ReadConfigDryContactPointOutReq",
-    136: "ReadConfigDryContactPointOutRsp",
-    9:   "TriggerDryContactPointOutReq",
-    137: "TriggerDryContactPointOutRsp",
+	5:   "ConfigDryContactPointOutReq",
+    133: "ConfigDryContactPointOutRsp",
+    6:   "ReadConfigDryContactPointOutReq",
+    134: "ReadConfigDryContactPointOutRsp",
+	7:   "TriggerDryContactPointOutReq",
+    135: "TriggerDryContactPointOutRsp",
 
   };
   return cfgcmdlist[cfgcmd];
@@ -41,40 +37,32 @@ function getCmdToID(cmdtype){
 	  return 4;
   else if (cmdtype == "GetShockSensorSensitivityRsp")
 	  return 132;
-  else if (cmdtype == "SetSoilTypeReq")
-	  return 5;
-  else if (cmdtype == "SetSoilTypeRsp")
-	  return 133;
-  else if (cmdtype == "GetSoilTypeReq")
-	  return 6;
-  else if (cmdtype == "GetSoilTypeRsp")
-	  return 134;
   else if (cmdtype == "ConfigDryContactPointOutReq")
-	  return 7;
+	  return 5;
   else if (cmdtype == "ConfigDryContactPointOutRsp")
-	  return 135;
+	  return 133;
   else if (cmdtype == "ReadConfigDryContactPointOutReq")
-	  return 8;
+	  return 6;
   else if (cmdtype == "ReadConfigDryContactPointOutRsp")
-	  return 136;
+	  return 134;
   else if (cmdtype == "TriggerDryContactPointOutReq")
-	  return 9;
+	  return 7;
   else if (cmdtype == "TriggerDryContactPointOutRsp")
-	  return 137;
+	  return 135;
 }
 
 function getDeviceName(dev){
   var deviceName = {
-	263: "R900PB02",
-	271: "R900PB02O1",
+	262: "R900PD01",
+	270: "R900PD01O1",
   };
   return deviceName[dev];
 }
 
 function getDeviceID(devName){
   var deviceName = {
-	"R900PB02": 263,
-	"R900PB02O1": 271,
+	"R900PD01": 262,
+	"R900PD01O1": 270,
   };
 
   return deviceName[devName];
@@ -106,35 +94,39 @@ function decodeUplink(input) {
 		}
 		
 		data.Device = getDeviceName((input.bytes[1]<<8 | input.bytes[2]));
-		if (input.bytes[4] & 0x80)
+		data.PH = (input.bytes[4]<<8 | input.bytes[5])/100+"pH";
+
+		if (input.bytes[6] & 0x80)
 		{
-			var tmp_v = input.bytes[4] & 0x7F;
-			data.Volt = (tmp_v / 10).toString() + '(low battery)';
+			var tmpval = (input.bytes[6]<<8 | input.bytes[7]);
+			data.TemperaturewithPH = (0x10000 - tmpval)/100 * -1+"℃";
 		}
 		else
-			data.Volt = input.bytes[4]/10;
+			data.TemperaturewithPH = (input.bytes[6]<<8 | input.bytes[7])/100+"℃";
 
-		data.Soil_VWC = (input.bytes[5]<<8 | input.bytes[6])/100+"%";
-
-		if (input.bytes[7] & 0x80)
+		data.NTU = (input.bytes[8]<<8 | input.bytes[9])/10+"ntu";
+		if (input.bytes[10] & 0x80)
 		{
-			var tmpval = (input.bytes[7]<<8 | input.bytes[8]);
-			data.Soil_Temperature = (0x10000 - tmpval)/100 * -1+"℃";
+			var tmpval = (input.bytes[10]<<8 | input.bytes[11]);
+			data.TemperaturewithNTU = (0x10000 - tmpval)/100 * -1+"℃";
 		}
 		else
-			data.Soil_Temperature = (input.bytes[7]<<8 | input.bytes[8])/100+"℃";
+			data.TemperaturewithNTU = (input.bytes[10]<<8 | input.bytes[11])/100+"℃";
+		data.Residual = (input.bytes[12]<<8 | input.bytes[13])/100+"mg/L";
 
-		data.Soil_EC = (input.bytes[9]<<8 | input.bytes[10])/1000+"ds/m";
-		
-		data.LowSoil_VWCAlarm = input.bytes[11] & 1;
-		data.HighSoil_VWCAlarm = input.bytes[11]>>1 & 1;
-		data.LowSoil_TemperatureAlarm = input.bytes[11]>>2 & 1;
-		data.HighSoil_TemperatureAlarm = input.bytes[11]>>3 & 1;
-		data.LowSoil_ECAlarm = input.bytes[11]>>4 & 1;
-		data.HighSoil_ECAlarm = input.bytes[11]>>5 & 1;
-		if(input.bytes[12] === 0x00){
+		data.LowPHAlarm = (input.bytes[14] | input.bytes[15])& 1;
+		data.HighPHAlarm = (input.bytes[14]| input.bytes[15])>>1 & 1;
+		data.LowNTUAlarm = (input.bytes[14]| input.bytes[15])>>2 & 1;
+		data.HighNTUAlarm = (input.bytes[14]| input.bytes[15])>>3 & 1;
+		data.LowResidualChlorineAlarm = (input.bytes[14]| input.bytes[15])>>4 & 1;
+		data.HighResidualChlorineAlarm = (input.bytes[14]| input.bytes[15])>>5 & 1;
+		data.LowTempWithPHAlarm = (input.bytes[14]| input.bytes[15])>>6 & 1;
+		data.HighTempWithPHAlarm = (input.bytes[14]| input.bytes[15])>>7 & 1;
+		data.LowTempWithNTUAlarm = (input.bytes[14]| input.bytes[15])>>8 & 1;
+		data.HighTempWithNTUAlarm = (input.bytes[14]| input.bytes[15])>>9 & 1;
+		if(input.bytes[16] === 0x00){
 			data.ShockTamperAlarm = 'NoAlarm';
-		}else if(input.bytes[12] === 0x01){
+		}else if(input.bytes[16] === 0x01){
 			data.ShockTamperAlarm = 'Alarm';
 		}
 
@@ -144,8 +136,7 @@ function decodeUplink(input) {
 		data.Cmd = getCfgCmd(input.bytes[0]);
 		data.Device = getDeviceName((input.bytes[1]<<8 | input.bytes[2]));
 		if (input.bytes[0] === getCmdToID("ConfigReportRsp")
-		|| input.bytes[0] === getCmdToID("SetShockSensorSensitivityRsp")
-		|| input.bytes[0] === getCmdToID("SetSoilTypeRsp"))
+		|| input.bytes[0] === getCmdToID("SetShockSensorSensitivityRsp"))
 		{
 			data.Status = (input.bytes[3] === 0x00) ? 'Success' : 'Failure';
 		}
@@ -153,33 +144,23 @@ function decodeUplink(input) {
 		{
 			data.MinTime = (input.bytes[3]<<8 | input.bytes[4]);
 			data.MaxTime = (input.bytes[5]<<8 | input.bytes[6]);
-			data.Soil_VWCChange = (input.bytes[7]<<8 | input.bytes[8])/100+"%";
-			data.Soil_TemperatureChange = (input.bytes[9]<<8 | input.bytes[10])/100+"℃";
-			data.Soil_ECChange = (input.bytes[11]<<8 | input.bytes[12])/1000+"ds/m";
+			data.PHChange = (input.bytes[7]<<8 | input.bytes[8])/100+"pH";
+			data.NTUChange = (input.bytes[9]<<8 | input.bytes[10])/10+"ntu";
+			data.ResidualChlorineChange = (input.bytes[11]<<8 | input.bytes[12])/100+"mg/L";
+			data.TemperatureChange = (input.bytes[13]<<8 | input.bytes[14])/100+"℃";
 		}
 		else if (input.bytes[0] === getCmdToID("GetShockSensorSensitivityRsp") )
 		{
 			data.ShockSensorSensitivity = input.bytes[3];
 		}
-		else if (input.bytes[0] === getCmdToID("GetSoilTypeRsp") )
-		{
-			if(input.bytes[3]===0x00){
-				data.SoilType = "Mineral Soil";
-			}else if(input.bytes[3]===0x01){
-				data.SoilType = "SandySoil";
-			}else if(input.bytes[3]===0x02){
-				data.SoilType = "Clay";
-			}else if(input.bytes[3]===0x03){
-				data.SoilType = "Organic soil";
-			}
-		}
-		if(data.Device === "R900PB02O1" && (
+		
+		if(data.Device === "R900PD01O1" && (
 			(input.bytes[0] === getCmdToID("ConfigDryContactPointOutRsp"))
 		|| (input.bytes[0] === getCmdToID("TriggerDryContactPointOutRsp"))
 		)){
 			data.Status = (input.bytes[3] === 0x00) ? 'Success' : 'Failure';
 		}
-		else if(data.Device === "R900PB02O1" && 
+		else if(data.Device === "R900PD01O1" && 
 		(input.bytes[0] === getCmdToID("ReadConfigDryContactPointOutRsp"))){
 			if(input.bytes[3] === 0x00){
 				data.DryContactPointOutType = 'NormallyLowLevel';
@@ -188,15 +169,20 @@ function decodeUplink(input) {
 			}
 			data.OutPluseTime = input.bytes[4]+ 's';
 
-			data.LowSoil_VWCAlarm = input.bytes[5] & 1;
-			data.HighSoil_VWCAlarm = input.bytes[5]>>1 & 1;
-			data.LowSoil_TemperatureAlarm = input.bytes[5]>>2 & 1;
-			data.HighSoil_TemperatureAlarm = input.bytes[5]>>3 & 1;
-			data.LowSoil_ECAlarm = input.bytes[5]>>4 & 1;
-			data.HighSoil_ECAlarm = input.bytes[5]>>5 & 1;
-			if(input.bytes[6] === 0x00){
+			data.LowPHAlarm = (input.bytes[5]| input.bytes[6]) & 1;
+			data.HighPHAlarm = (input.bytes[5]| input.bytes[6])>>1 & 1;
+			data.LowNTUAlarm = (input.bytes[5]| input.bytes[6])>>2 & 1;
+			data.HighNTUAlarm = (input.bytes[5]| input.bytes[6])>>3 & 1;
+			data.LowResidualChlorineAlarm = (input.bytes[5]| input.bytes[6])>>4 & 1;
+			data.HighResidualChlorineAlarm = (input.bytes[5]| input.bytes[6])>>5 & 1;
+			data.LowTempWithPHAlarm = (input.bytes[5]| input.bytes[6])>>6 & 1;
+			data.HighTempWithPHAlarm = (input.bytes[5]| input.bytes[6])>>7 & 1;
+			data.LowTempWithNTUAlarm = (input.bytes[5]| input.bytes[6])>>8 & 1;
+			data.HighTempWithNTUAlarm = (input.bytes[5]| input.bytes[6])>>9 & 1;
+
+			if(input.bytes[7] === 0x00){
 				data.Channel = 'Channel1';
-			}else if(input.bytes[6] === 0x01){
+			}else if(input.bytes[8] === 0x01){
 				data.Channel = 'Channel2';
 			}
 		}
@@ -226,16 +212,17 @@ function encodeDownlink(input) {
   {
 	  var mint = input.data.MinTime;
 	  var maxt = input.data.MaxTime;
-	  var sVWC = input.data.Soil_VWCChange*100;
-	  var sTemp = input.data.Soil_TemperatureChange*100;
-	  var sEC = input.data.Soil_ECChange*1000;
-	  
+	  var ph = input.data.PHChange*100;
+	  var ntu = input.data.NTUChange*10;
+	  var residual = input.data.ResidualChlorineChange*100;
+	  var temperature = input.data.TemperatureChange*100;
+
 	  ret = ret.concat(getCmdID, (devid>>8), (devid & 0xFF), (mint >> 8), (mint & 0xFF), (maxt >> 8), (maxt & 0xFF), 
-	   (sVWC >> 8), (sVWC & 0xFF),  (sTemp >> 8), (sTemp & 0xFF),  (sEC >> 8), (sEC & 0xFF));
+	   (ph >> 8), (ph & 0xFF),  (ntu >> 8), (ntu & 0xFF), 
+	   (residual >> 8), (residual & 0xFF), (temperature >> 8), (temperature & 0xFF));
   }
   else if (input.data.Cmd == "ReadConfigReportReq" 
-  	|| input.data.Cmd == "GetShockSensorSensitivityReq" 
-	|| input.data.Cmd == "GetSoilTypeReq")
+  	|| input.data.Cmd == "GetShockSensorSensitivityReq" )
   {
 	  ret = ret.concat(getCmdID, (devid>>8), (devid & 0xFF));
   }
@@ -244,22 +231,7 @@ function encodeDownlink(input) {
 	  var sSensor = input.data.ShockSensorSensitivity;
 	  ret = ret.concat(getCmdID, (devid>>8), (devid & 0xFF),sSensor);
   }  
-  else if (input.data.Cmd == "SetSoilTypeReq")
-  {
-	  var sSensor = input.data.SoilType;
-	  var value;
-	  if(sSensor==="Mineral Soil"){
-		value = 0x00;
-	  }else if(sSensor==="SandySoil"){
-		value = 0x01;
-	  }else if(sSensor==="Clay"){
-		value = 0x02;
-	  }else if(sSensor==="Organic soil"){
-		value = 0x03;
-	}
-	  ret = ret.concat(getCmdID, (devid>>8), (devid & 0xFF),value);
-  }  
-  if(devid===271 && input.data.Cmd == "ConfigDryContactPointOutReq"){
+  if(devid===270 && input.data.Cmd == "ConfigDryContactPointOutReq"){
 	 var dryCont = input.data.DryContactPointOutType;
 	 var dryContValue;
 	 if(dryCont==="NormallyLowLevel"){
@@ -268,13 +240,20 @@ function encodeDownlink(input) {
 		dryContValue = 0x01;
 	  }
 	  var outpluse = input.data.OutPluseTime;
-	  var lowSVWC = input.data.LowSoil_VWCAlarm;
-	  var highSVWC = input.data.HighSoil_VWCAlarm;
-	  var lowSTemp = input.data.LowSoil_TemperatureAlarm;
-	  var highSTemp = input.data.HighSoil_TemperatureAlarm;
-	  var lowSEC = input.data.LowSoil_ECAlarm;
-	  var highSEC = input.data.HighSoil_ECAlarm;
-	  var bindAS = lowSVWC | highSVWC<<1 | lowSTemp<<2 | highSTemp<<3 | lowSEC<<4 | highSEC<<5
+
+	  var lowph = input.data.LowPHAlarm;
+	  var highph = input.data.HighPHAlarm;
+	  var lowntu = input.data.LowNTUAlarm;
+	  var highntu = input.data.HighNTUAlarm;
+	  var lowres = input.data.LowResidualChlorineAlarm;
+	  var highres = input.data.HighResidualChlorineAlarm;
+	  var lowtempph = input.data.LowTempWithPHAlarm;
+	  var hightempph = input.data.HighTempWithPHAlarm;
+	  var lowtempntu = input.data.LowTempWithNTUAlarm;
+	  var hightempntu = input.data.HighTempWithNTUAlarm;
+	  var bindAS = lowph | highph<<1 | lowntu<<2 | highntu<<3 | lowres<<4 | highres<<5 
+	  | lowtempph<<6 | hightempph<<7 | lowtempntu<<8 | hightempntu<<9
+	
 	  var channel = input.data.Channel;
 	  var channelValue;
 	  if(channel==="Channel1"){
@@ -282,9 +261,9 @@ function encodeDownlink(input) {
 	   }else if(channel==="Channel2"){
 		channelValue = 0x01;
 	   }
-	  ret = ret.concat(getCmdID, (devid>>8), (devid & 0xFF),dryContValue,outpluse,bindAS,channelValue);
+	  ret = ret.concat(getCmdID, (devid>>8), (devid & 0xFF),dryContValue,outpluse,(bindAS>>8), (bindAS & 0xFF),channelValue);
   }
-  if(devid===271 && input.data.Cmd == "ReadConfigDryContactPointOutReq"){
+  if(devid===270 && input.data.Cmd == "ReadConfigDryContactPointOutReq"){
 	 var channel = input.data.Channel;
 	 var channelValue;
 	 if(channel==="Channel1"){
@@ -294,7 +273,7 @@ function encodeDownlink(input) {
 	  }
 	 ret = ret.concat(getCmdID, (devid>>8), (devid & 0xFF),channelValue);
  }
- if(devid===271 && input.data.Cmd == "TriggerDryContactPointOutReq"){
+ if(devid===270 && input.data.Cmd == "TriggerDryContactPointOutReq"){
 	var outpluse = input.data.OutPluseTime;
 	var channel = input.data.Channel;
 	var channelValue;
@@ -305,6 +284,7 @@ function encodeDownlink(input) {
 	 }
 	ret = ret.concat(getCmdID, (devid>>8), (devid & 0xFF),outpluse,channelValue);
 }
+
   return {
     fPort: 23,
     bytes: ret
@@ -321,26 +301,17 @@ function decodeDownlink(input) {
 		{
 			data.MinTime = (input.bytes[3]<<8 | input.bytes[4]);
 			data.MaxTime = (input.bytes[5]<<8 | input.bytes[6]);
-			data.Soil_VWCChange = (input.bytes[7]<<8 | input.bytes[8])/100+"%";
-			data.Soil_TemperatureChange = (input.bytes[9]<<8 | input.bytes[10])/100+"℃";
-			data.Soil_ECChange = (input.bytes[11]<<8 | input.bytes[12])/1000+"ds/m";
+			data.PHChange = (input.bytes[7]<<8 | input.bytes[8])/100+"pH";
+			data.NTUChange = (input.bytes[9]<<8 | input.bytes[10])/10+"ntu";
+			data.ResidualChlorineChange = (input.bytes[11]<<8 | input.bytes[12])/100+"mg/L";
+			data.TemperatureChange = (input.bytes[13]<<8 | input.bytes[14])/100+"℃";
 		}
 		else if (input.bytes[0] === getCmdToID("SetShockSensorSensitivityReq"))
 		{
 			data.ShockSensorSensitivity = input.bytes[3];
-		}else if(input.bytes[0] === getCmdToID("SetSoilTypeReq")){
-			if(input.bytes[3]===0x00){
-				data.SoilType = "Mineral Soil";
-			}else if(input.bytes[3]===0x01){
-				data.SoilType = "SandySoil";
-			}else if(input.bytes[3]===0x02){
-				data.SoilType = "Clay";
-			}else if(input.bytes[3]===0x03){
-				data.SoilType = "Organic soil";
-			}
 		}
 
-		if (data.Device=="R900PB02O1" && input.bytes[0] === getCmdToID("ConfigDryContactPointOutReq"))
+		if (data.Device=="R900PD01O1" && input.bytes[0] === getCmdToID("ConfigDryContactPointOutReq"))
 		{
 			if(input.bytes[3] === 0x00){
 				data.DryContactPointOutType = 'NormallyLowLevel';
@@ -349,26 +320,32 @@ function decodeDownlink(input) {
 			}
 			data.OutPluseTime = input.bytes[4]+ 's';
 
-			data.LowSoil_VWCAlarm = input.bytes[5] & 1;
-			data.HighSoil_VWCAlarm = input.bytes[5]>>1 & 1;
-			data.LowSoil_TemperatureAlarm = input.bytes[5]>>2 & 1;
-			data.HighSoil_TemperatureAlarm = input.bytes[5]>>3 & 1;
-			data.LowSoil_ECAlarm = input.bytes[5]>>4 & 1;
-			data.HighSoil_ECAlarm = input.bytes[5]>>5 & 1;
-			if(input.bytes[6] === 0x00){
+			data.LowPHAlarm = (input.bytes[5]| input.bytes[6]) & 1;
+			data.HighPHAlarm = (input.bytes[5]| input.bytes[6])>>1 & 1;
+			data.LowNTUAlarm = (input.bytes[5]| input.bytes[6])>>2 & 1;
+			data.HighNTUAlarm = (input.bytes[5]| input.bytes[6])>>3 & 1;
+			data.LowResidualChlorineAlarm = (input.bytes[5]| input.bytes[6])>>4 & 1;
+			data.HighResidualChlorineAlarm = (input.bytes[5]| input.bytes[6])>>5 & 1;
+			data.LowTempWithPHAlarm = (input.bytes[5]| input.bytes[6])>>6 & 1;
+			data.HighTempWithPHAlarm = (input.bytes[5]| input.bytes[6])>>7 & 1;
+			data.LowTempWithNTUAlarm = (input.bytes[5]| input.bytes[6])>>8 & 1;
+			data.HighTempWithNTUAlarm = (input.bytes[5]| input.bytes[6])>>9 & 1;
+
+
+			if(input.bytes[7] === 0x00){
 				data.Channel = 'Channel1';
-			}else if(input.bytes[6] === 0x01){
+			}else if(input.bytes[7] === 0x01){
 				data.Channel = 'Channel2';
 			}
 		}
-		else if(data.Device=="R900PB02O1" && input.bytes[0] === getCmdToID("ReadConfigDryContactPointOutReq")){
+		else if(data.Device=="R900PD01O1" && input.bytes[0] === getCmdToID("ReadConfigDryContactPointOutReq")){
 			if(input.bytes[3] === 0x00){
 				data.Channel = 'Channel1';
 			}else if(input.bytes[3] === 0x01){
 				data.Channel = 'Channel2';
 			}
 		}
-		else if(data.Device=="R900PB02O1" && input.bytes[0] === getCmdToID("TriggerDryContactPointOutReq")){
+		else if(data.Device=="R900PD01O1" && input.bytes[0] === getCmdToID("TriggerDryContactPointOutReq")){
 			data.OutPluseTime = input.bytes[3]
 			if(input.bytes[4] === 0x00){
 				data.Channel = 'Channel1';
