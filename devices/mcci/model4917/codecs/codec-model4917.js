6b7ce8b168c23023658d539e1e5c24310e21bc44
johanstokking <johan@stokking.nl>
2026-06-29 11:51:15 +0000

    Deploying to gh-pages from  @ 836fcdc87954025b3ccf540c99840bd4c4fb1139 🚀

--- devices/mcci/model4917/codecs/codec-model4917.js
+++ devices/mcci/model4917/codecs/codec-model4917.js
@@ -245,7 +245,7 @@ function DecodeDownlinkResponse(bytes) {
     else decoded.timeType = 'boot time';
 
     timestamp = (timestamp >> 1) * 2;
-    decoded.timestamp = new Date((timestamp + /* gps epoch to posix */ 315964800 - /* leap seconds */ 17) * 1000);
+    decoded.timestamp = new Date((timestamp + /* gps epoch to posix */ 315964800 - /* leap seconds */ 17) * 1000).toISOString();
     var iBoot = bytes[Parse.i++];
     decoded.boot = iBoot;
     decoded.tProbeBottom = DecodeI16(Parse) / 256;
@@ -292,7 +292,7 @@ function Decoder(bytes, port) {
     else decoded.timeType = 'boot time';
 
     timestamp = (timestamp >> 1) * 2;
-    decoded.timestamp = new Date((timestamp + /* gps epoch to posix */ 315964800 - /* leap seconds */ 17) * 1000);
+    decoded.timestamp = new Date((timestamp + /* gps epoch to posix */ 315964800 - /* leap seconds */ 17) * 1000).toISOString();
   }
 
   if (flags & 0x4) {
